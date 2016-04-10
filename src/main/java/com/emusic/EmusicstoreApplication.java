f14675ae6de4f94d3ca583ab04f4dbffc3adc3b6
Francis Phiri <franzparks@gmail.com>
2016-04-10 09:18:33 -0700

    Add initial project files
    -initialize project with spring boot

---
+++ src/main/java/com/emusic/EmusicstoreApplication.java
@@ -0,0 +1,16 @@
+package com.emusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author francisphiri
+ *
+ */
+@SpringBootApplication
+public class EmusicstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmusicstoreApplication.class, args);
+	}
+}
