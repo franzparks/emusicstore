848c803bc2bba9699eba552864e7dbb079e3add7
franzparks <franzparks@gmail.com>
2016-04-14 15:32:29 -0700

    feat: Add data retrieval service

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,16 +1,27 @@
+//Services
+angular.module("getProducts.services", ["ngResource"]).
+ factory('Product', function ($resource) {
+       var Product = $resource('/products/:productId',{ productId:'@id'});
+         Product.prototype.isNew = function(){
+        return (typeof(this.id) === 'undefined');
+      }
+     return Product;
+    });
 //Apps
-var app = angular.module('eMusicStore', ['ngRoute']);
+var app = angular.module('eMusicStore', ['ngRoute',getProducts.services]);
+ 
+
 app.config(function($routeProvider){
-	$routeProvider.
-	    when('/',{templateUrl: 'views/carousel.html'}).
-		when('/about',{templateUrl: 'views/about.html'}).
-		when('/products',{templateUrl: 'views/productList.html',controller: 'ProductsCtrl'}).
-		when('/product/viewProduct/1',{templateUrl: 'views/viewProduct.html',controller: 'ProductCtrl'}).
+	$routeProvider
+	    .when('/',{templateUrl: 'views/carousel.html'})
+		.when('/about',{templateUrl: 'views/about.html'})
+		.when('/products',{templateUrl: 'views/productList.html',controller: 'ProductListCtrl'})
+		.when('/products/:productId',{templateUrl: 'views/viewProduct.html',controller: 'ProductDetailsCtrl'})
+        .when('/products/new', {templateUrl: 'views/products/addProduct.html', controller: 'ProductCreateCtrl'})
+		
 		otherwise({ redirectTo: '/' });
 
 });
-
-
 
 
 //Controllers
@@ -19,12 +30,16 @@ app.controller('MainController', function($scope) {
 	$scope.text = "Hello World!!!!";
 });
 
-app.controller('ProductsCtrl', function($scope) {
+app.controller('ProductListCtrl', function($scope,Product,$resource) {
 	$scope.products = [
 	    {"productId":"1", "productName":"Guiter", "productCategory":"Instruments","productCondition": "New", "productPrice": "230"},
 	    {"productId":"1", "productName":"Grand Piano", "productCategory":"Instruments","productCondition": "New", "productPrice": "2330"} 
 	   
 	]
+	
+	 $scope.products = Product.query();
+	
+	
 });
 
 app.controller('ProductCtrl', function($scope) {
@@ -32,3 +47,22 @@ app.controller('ProductCtrl', function($scope) {
 			
  "productCategory":"Instruments","productCondition": "New", "productPrice": "2330"};
 });
+
+ 
+app.controller('ProductCreateCtrl', function($scope, $routeParams, $location, Product) {
+ 
+    $scope.product = new Product();
+ 
+    $scope.save = function () {
+        $scope.product.$save(function (product, headers) {
+            toastr.success("Submitted New Product");
+            $location.path('/');
+        });
+    };
+});
+ 
+ 
+app.controller('ProductDetailsCtrl', function($scope, $routeParams, $location, Product) {
+    var productId = $routeParams.productId;
+    $scope.product = Product.get({productId: productId});
+}
