2ce3cdf6242f09b496ef057d044b122876c52f0c
franzparks <franzparks@gmail.com>
2016-04-18 15:46:21 -0700

    chore: Add annotation to make the link to Facebook

--- src/main/java/com/emusic/EmusicstoreApplication.java
+++ src/main/java/com/emusic/EmusicstoreApplication.java
@@ -2,12 +2,14 @@ package com.emusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 /**
  * 
  * @author francisphiri
  *
  */
 @SpringBootApplication
+@EnableOAuth2Sso
 public class EmusicstoreApplication {
 
 	public static void main(String[] args) {
