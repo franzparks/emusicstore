80301bf1647bca48d5fafe9b083c57953b81a6f9
franzparks <franzparks@gmail.com>
2016-04-19 13:45:45 -0700

    feature: switch off the security on the home page

---
+++ src/main/java/com/emusic/SecurityConfig.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.emusic;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author francis
+ *
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	      .antMatcher("/**")
+	      .authorizeRequests()
+	        .antMatchers("/", "/login**", "/webjars/**")
+	        .permitAll()
+	      .anyRequest()
+	        .authenticated();
+	  }
+
+}
