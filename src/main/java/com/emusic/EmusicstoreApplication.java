cf3080302c7192be28470a91ce39080744052777
franzparks <franzparks@gmail.com>
2016-04-19 14:04:36 -0700

    refactor: read config class for security

--- src/main/java/com/emusic/EmusicstoreApplication.java
+++ src/main/java/com/emusic/EmusicstoreApplication.java
@@ -3,17 +3,21 @@ package com.emusic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 /**
  * 
  * @author francisphiri
  *
  */
 @SpringBootApplication
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class EmusicstoreApplication{
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EmusicstoreApplication.class, args);
 	}
+	
 	
 }
