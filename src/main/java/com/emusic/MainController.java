a1187773b6d0dd5a130a5d52ae6349d2d53a3109
franzparks <franzparks@gmail.com>
2016-04-21 15:45:56 -0700

    refactor: Add controller functionality to MainController class

--- src/main/java/com/emusic/MainController.java
+++ src/main/java/com/emusic/MainController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 //@CrossOrigin(origins = "http://localhost:8080")
 public class MainController {
 	
-	//@RequestMapping("/user")
+	  @RequestMapping("/user")
 	  public Principal user(Principal principal) {
 	    return principal;
 	  }
