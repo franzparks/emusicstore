789b8cb538614c8040cd0482aa7ef81dd254558c
franzparks <franzparks@gmail.com>
2016-04-22 19:52:49 -0700

    chore: reenable other routes

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -84,11 +84,11 @@ app.config(function($routeProvider,$httpProvider){
 	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 	$routeProvider
 	    
-		//.when('/about',{templateUrl: '/views/about.html', controller : "ProductDetailsCtrl"})
+		.when('/about',{templateUrl: '/views/about.html'})
 		.when('/products',{templateUrl: "/views/productList.html", controller : "ProductListCtrl"})
-		//.when('/products/product/details',{templateUrl: 'views/viewProduct.html',controller: 'ProductListCtrl'})
+		.when('/products/product/details',{templateUrl: 'views/viewProduct.html',controller: 'ProductListCtrl'})
         //.when('/products/new', {templateUrl: 'views/addProduct.html', controller: 'ProductCreateCtrl'})
-        //.when('/customer/cart', {templateUrl: '/views/cart.html', controller: 'cartCtrl'})
+        .when('/customer/cart', {templateUrl: '/views/cart.html', controller: 'cartCtrl'})
 		.otherwise({ redirectTo: '/' });
 	
 	
